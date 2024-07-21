adc47ab5933ca21d344f46f0dc83fb6ac3455f3f
rhuamanh <rhuamanh@unjbg.edu.pe>
2024-07-21 18:10:56 -0500

    Obtencion de datos de n personas

--- registro_prueba.cpp
+++ registro_prueba.cpp
@@ -6,4 +6,21 @@ struct persona{
 	string name;
 	int edad;
 	string pasaTiempo;
-}persona [3];
+}persona[1000];
+
+int main (){
+	int n;
+	cout<<"ingrese el numero de personas:";
+	cin>>n;
+	for (int i=0; i<n; i++){
+		cout<<"--DATOS DE LA PERSONA N°"<<i+1<<"--"<<endl;
+		cout<<"\n==>Ingrese su nombre:";
+		cin>>persona[i].name;
+		cout<<"==>Ingrese su edad:";
+		cin>>persona[i].edad;
+		cout<<"==>Ingrese el pasatiempo:";
+		cin>>persona[i].pasaTiempo;
+		cout<<"\n";
+	}
+	return 0;
+}
